226634984ae161abfa0b3c1272b2bdf089bb6a69
Domitrius Clark <domitriusaclark@gmail.com>
2023-07-10 21:01:40 -0400

    added a check to make sure home takes you to the right home page

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar({ isLoggedIn }: Props) {
   return (
     <nav className="flex flex-col items-center pt-10 pr-10">
       <div className="flex self-end gap-10">
-        <Link href="/">Home </Link>
+        <Link href={isLoggedIn ? "/dashboard" : "/"}>Home </Link>
         {isLoggedIn ? (
           <UserButton afterSignOutUrl="/" />
         ) : (
