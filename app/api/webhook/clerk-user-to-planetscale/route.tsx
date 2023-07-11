0eff89c6d4f51c1b87dedd0d52b689128c5d667c
Domitrius Clark <domitriusaclark@gmail.com>
2023-07-10 23:16:40 -0400

    More live webhook testing cause im too lazy to setup ngrok

--- app/api/webhook/clerk-user-to-planetscale/route.tsx
+++ app/api/webhook/clerk-user-to-planetscale/route.tsx
@@ -1,9 +1,8 @@
 import type { WebhookEvent } from "@clerk/clerk-sdk-node";
 
-export async function POST(req: Request) {
-  // @ts-expect-error
-  const event = req.body?.event as WebhookEvent;
-
+export async function POST(req: Request, event: Event) {
+  console.log(req.body);
+  console.log(req.headers);
   console.log(event);
 
   return new Response("OK");
